0486439ff43a95c6b5a61e795a385761e93f3357
mur4ik18 <o.o.kotov1999@gmail.com>
2021-03-11 22:54:40 +0200

    template 0.1

---
+++ run.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+
+# https://tilda.cc/page/?pageid=15032430&previewmode=yes#rec247887616
+
+
+app = Flask('__main__',static_folder="static")
+
+
+menu = ['О нас','Этапы', 'Дополнительные услуги', 'Галерея','Контакты']
+
+section1 = {
+    'Title' : 'БЫСТРО, ДОСТУПНО, КАЧЕСТВЕННО',
+    'plist' : ['ПВХ окна','балконы','входные двери','межкомнатные двери','роллеты','жалюзи всех типов','3Д-панели']
+}
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Try to leaern it', menu=menu, section1=section1)
+
+if __name__ == '__main__':
+    app.run(debug=True)
